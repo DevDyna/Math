06ed73f72351226503bb8b09377bb910f50c9e4d
DevDyna <andrearisorti2003@gmail.com>
2024-10-15 20:32:36 +0200

    Update Tag_Chem.js

--- kubejs/server_scripts/Tag_Chem.js
+++ kubejs/server_scripts/Tag_Chem.js
@@ -6,7 +6,7 @@ let ChemTag = (event) => {
   let valid_ingot;
   global.chem.name.forEach((element_name) => {
     event.add("kubejs:elements", "chemlib:" + element_name);
-    global.chem.mods.forEach((mod_name) => {
+    global.utils.mods.forEach((mod_name) => {
       valid_ingot =
         mod_name == "mekanism"
           ? Item.of(mod_name + ":ingot_" + element_name)
