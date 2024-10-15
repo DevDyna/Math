946feff068d6ed7a7324a345359bf0c2565e2c98
DevDyna <andrearisorti2003@gmail.com>
2024-10-15 20:32:39 +0200

    Update Recipe_Chem.js

--- kubejs/server_scripts/Recipe_Chem.js
+++ kubejs/server_scripts/Recipe_Chem.js
@@ -8,7 +8,7 @@ let ChemConversion = (event) => {
   let element_1 = Array.from(global.chem.name);
   let element_2 = Array.from(global.chem.name);
 
-  let mod_list = ["mekanism", "xycraft_world", "minecraft", "chemlib"];
+  //let mod_list = ["mekanism", "xycraft_world", "minecraft", "chemlib"];
 
   // CHEM NUMBER -> ELEMENT
   for (let i = 1; i < crafting_number_max + 1; i++) {
@@ -17,7 +17,7 @@ let ChemConversion = (event) => {
       "kubejs:" + i,
     ]);
 
-    mod_list.forEach((mod_list_1) => {
+    global.utils.mods.forEach((mod_list_1) => {
       //SHAPELESS PAXEL INGOT -> PLATE
       if (
         Item.of("chemlib:" + element_0[i] + "_plate") != "minecraft:air" &&
@@ -38,7 +38,7 @@ let ChemConversion = (event) => {
         element_2.splice(element_2.indexOf(element_0[i]), 1);
       }
 
-      mod_list.forEach((mod_list_2) => {
+      global.utils.mods.forEach((mod_list_2) => {
         //SMELTING DUST -> INGOT
         if (
           Item.of(mod_list_2 + ":" + element_0[i] + "_dust") !=
