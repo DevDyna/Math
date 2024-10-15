863c6f3e0b621e90043889eb24c3afdb99199a9f
DevDyna <andrearisorti2003@gmail.com>
2024-10-15 18:15:56 +0200

    Create Tag_Chem.js

---
+++ kubejs/server_scripts/Tag_Chem.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param {TagEvent.Item} event
+ */
+let ChemTag = (event) => {
+  let valid_ingot;
+  global.chem.name.forEach((element_name) => {
+    event.add("kubejs:elements", "chemlib:" + element_name);
+    global.chem.mods.forEach((mod_name) => {
+      valid_ingot =
+        mod_name == "mekanism"
+          ? Item.of(mod_name + ":ingot_" + element_name)
+          : Item.of(mod_name + ":" + element_name + "_ingot");
+      if (valid_ingot != "minecraft:air") {
+        event.add("kubejs:chem_ingots", valid_ingot);
+      }
+    });
+  });
+};
