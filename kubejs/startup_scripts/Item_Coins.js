630e829bcb4c77d0d5c93495d5ce87278d295702
DevDyna <andrearisorti2003@gmail.com>
2024-10-16 19:14:22 +0200

    Create Item_Coins.js

---
+++ kubejs/startup_scripts/Item_Coins.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * @param {Registry.Item} event
+ */
+let CoinsInit = (event) => {
+  let coins = {
+    name: ["bronze", "silver", "gold", "platinum"],
+    hex: [0xcd7f32, 0xc0c0c0, 0xffd700, 0xe5e4e2],
+  };
+
+  coins.name.forEach((e, index) => {
+    event
+      .create(e + "_coin")
+      .modelJson({
+        parent: "minecraft:item/generated",
+        textures: {
+          layer0: "kubejs:item/coin",
+        },
+      })
+      .color(0, coins.hex[index])
+      .tag("kubejs:coins")
+      .tooltip(
+        "§7[:" +
+          (index == 0 ? "§f" : "§7") +
+          "1§7:|:" +
+          (index == 1 ? "§f" : "§7") +
+          "2§7:|:" +
+          (index == 2 ? "§f" : "§7") +
+          "3§7:|:" +
+          (index == 3 ? "§f" : "§7") +
+          "4§7:]"
+      );
+  });
+};
