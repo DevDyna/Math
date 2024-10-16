75fa07ed062bbf78be415b190f55e33104a57fae
DevDyna <andrearisorti2003@gmail.com>
2024-10-16 19:14:12 +0200

    Create lang.js

---
+++ kubejs/client_scripts/lang.js
@@ -0,0 +1,6 @@
+ClientEvents.lang('en_us',event=>{
+    event.add('bountiful.decree.chem_core.name',"Chemical Reaction")
+    event.add('bountiful.decree.math_core.name',"Theory of Marketing")
+    event.add('bountiful.decree.operator_core.name',"Analog Logic")
+    event.add('bountiful.decree.other_core.name',"Yellow as Money")
+})
