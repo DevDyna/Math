24a7dc8c50186b318da0a0cc7529c9ad759c2a53
DevDyna <andrearisorti2003@gmail.com>
2024-10-16 21:38:26 +0200

    Update Recipe_Remove.js

--- kubejs/server_scripts/Recipe_Remove.js
+++ kubejs/server_scripts/Recipe_Remove.js
@@ -4,12 +4,41 @@
  * @param {Internal.RecipesEventJS} event
  */
 let removedRecipes = (event) => {
-  //BADRECIPES(event)
+
+
+  let recipe_ids =[
+    'projectred_expansion:battery',
+    'projectred_expansion:electric_screwdriver',
+    'projectred_core:multimeter',
+    'projectred_expansion:frame',
+    'projectred_expansion:deployer',
+    'projectred_expansion:block_breaker',
+    'projectred_expansion:frame_actuator',
+   'projectred_expansion:frame_motor',
+    'projectred_expansion:fire_starter',
+    'projectred_expansion:auto_crafter',
+    'projectred_expansion:battery_box',
+    'projectred_expansion:project_bench',
+    'projectred_core:silicon',
+    'projectred_core:electrotine_generator',
+    'projectred_core:plate',
+  ]
+
+
   event.remove({
     not: [
       { type: "alchemistry:combiner" },
       { type: "alchemistry:dissolver" },
       { type: "alchemistry:compactor" },
+      { mod: "projectred_transmission" },
+      { mod: "projectred_integration" },
+      { mod: "projectred_illumination" },
+      { mod: "projectred_expansion" },
+      { mod: "projectred_core" },
     ],
   });
+  recipe_ids.forEach(e=>{
+    event.remove({id:e})
+  })
+  
 };
