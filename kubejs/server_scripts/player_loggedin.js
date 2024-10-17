f6656c779d22bc438668adc98f8cd6693b3ced35
DevDyna <andrearisorti2003@gmail.com>
2024-10-18 01:31:54 +0200

    Update player_loggedin.js

--- kubejs/server_scripts/player_loggedin.js
+++ kubejs/server_scripts/player_loggedin.js
@@ -1,4 +1,4 @@
-ServerEvents.loaded(event => {
+PlayerEvents.loggedIn(event => {
         event.server.runCommandSilent(`gamerule doMobSpawning false`)
         event.server.runCommandSilent(`gamerule doTraderSpawning false`)
         event.server.runCommandSilent(`gamerule doFireTick true`)
