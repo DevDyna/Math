2d27deaf7d0b7f2a45c017d375a6a21671a65255
DevDyna <andrearisorti2003@gmail.com>
2024-10-18 01:31:52 +0200

    Update jei.js

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -1,7 +1,7 @@
 const $MekanismJEI = Java.loadClass("mekanism.client.jei.MekanismJEI");
 JEIEvents.hideCustom((event) => {
-  event.get($MekanismJEI.TYPE_GAS).hideAll();
-  event.get($MekanismJEI.TYPE_INFUSION).hideAll();
+  // event.get($MekanismJEI.TYPE_GAS).hideAll();
+  // event.get($MekanismJEI.TYPE_INFUSION).hideAll();
   event.get($MekanismJEI.TYPE_PIGMENT).hideAll();
   event.get($MekanismJEI.TYPE_SLURRY).hideAll();
 });
