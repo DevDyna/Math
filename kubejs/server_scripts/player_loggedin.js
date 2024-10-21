9ae191505ae33d4d785f52425bb2af5c4be4a6ee
DevDyna <andrearisorti2003@gmail.com>
2024-10-21 02:33:20 +0200

    Delete player_loggedin.js

--- kubejs/server_scripts/player_loggedin.js
+++
@@ -1,5 +0,0 @@
-PlayerEvents.loggedIn(event => {
-        event.server.runCommandSilent(`gamerule doMobSpawning false`)
-        event.server.runCommandSilent(`gamerule doTraderSpawning false`)
-        event.server.runCommandSilent(`gamerule doFireTick true`)
-})
