022ae510e813c486900def9c28f7551ea47f1681
DevDyna <andrearisorti2003@gmail.com>
2024-11-05 21:22:01 +0100

    Update _Master.js

--- kubejs/server_scripts/_Master.js
+++ kubejs/server_scripts/_Master.js
@@ -1,41 +1,25 @@
 // priority: -1
 
-// /**
-//  *
-//  * @param {Internal.RecipesEventJS} event
-//  */
-// let numToChem=(event)=>{}
 ServerEvents.recipes((event) => {
   removedRecipes(event);
   commonRecipes(event);
-  OperatorRecipes(event);
+   OperatorRecipes(event);
   ChemConversion(event);
   LogFactoryRecipe(event);
   specialRecipes(event);
 });
 
-// /**
-//  *
-//  * @param {TagEvent.Item} event
-//  */
-// let ChemTag=(event)=>{}
 ServerEvents.tags("item", (event) => {
   ChemTag(event);
   BrokenFixerTag(event);
 });
 
-
-// /**
-//  *
-//  * @param {Internal.DataPackEventJS} event
-//  */
-// let ChemTag=(event)=>{}
 ServerEvents.lowPriorityData((event) => {
-
-  //Bounty_Remove(event)
   Bounty_Reward(event)
   Bounty_Quest(event)
   Bounty_Core(event)
-
-
 })
+
+PlayerEvents.loggedIn((event) => {
+  PlayerLogin(event);
+});
