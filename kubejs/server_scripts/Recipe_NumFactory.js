83bf4d7bc44c55071d2b61cb4585383dfff29557
DevDyna <andrearisorti2003@gmail.com>
2024-11-09 20:00:10 +0100

    Update Recipe_NumFactory.js

--- kubejs/server_scripts/Recipe_NumFactory.js
+++ kubejs/server_scripts/Recipe_NumFactory.js
@@ -3,26 +3,6 @@
  * @param {Internal.RecipesEventJS} event
  */
 let OperatorRecipes = (event) => {
-  let uncraft = (input, outputs) =>
-    event.custom({
-      type: "twilightforest:uncrafting",
-      cost: 0,
-      input: {
-        ingredient: {
-          item: input,
-        },
-      },
-      key: {
-        A: {
-          item: outputs[0],
-        },
-        B: {
-          item: outputs[1],
-        },
-      },
-      pattern: ["AB"],
-    });
-
   /**
    *
    * @param {[string,string]} inputs
@@ -70,14 +50,10 @@ let OperatorRecipes = (event) => {
   for (let i_result = 1; i_result < crafting_number_max + 1; i_result++) {
     for (let i_add1 = 0; i_add1 < crafting_number_max + 1; i_add1++) {
       for (let i_add2 = 0; i_add2 < crafting_number_max + 1; i_add2++) {
+        
         if (i_result == i_add1 + i_add2) {
           op_plus([i_add1, i_add2], i_result);
           op_less([i_result, i_add2], i_add1);
-
-          uncraft("kubejs:" + i_result, [
-            "kubejs:" + i_add1,
-            "kubejs:" + i_add2,
-          ]);
         }
 
         if (i_result == i_add1 * i_add2) {
@@ -88,5 +64,3 @@ let OperatorRecipes = (event) => {
     }
   }
 };
-
-
